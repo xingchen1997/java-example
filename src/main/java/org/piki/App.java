f11b2a4fee7c14c78979f3e298032187e410c9e7
Patrick Reynolds <patrick@piki.org>
2022-03-24 17:20:14 -0400

    Check in Fibonacci function and tests
    One of the tests is failing!

--- src/main/java/org/piki/App.java
+++ src/main/java/org/piki/App.java
@@ -2,6 +2,22 @@ package org.piki;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Here are some Fibonacci numbers:");
+        for (int i=1; i<=10; i++) {
+            System.out.println(i + ": " + fibonacci(i));
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 2) return 1;
+
+        int a = 1, b = 1;
+        for (int i=2; i<n; i++) {
+            int t = a;
+            a = b;
+            b = b + t;
+        }
+
+        return b;
     }
 }
--- src/test/java/org/piki/AppTest.java
+++ src/test/java/org/piki/AppTest.java
@@ -14,6 +14,16 @@ public class AppTest extends TestCase {
     }
 
     public void testApp() {
-        assertTrue(false);
+        assertTrue(true);
+    }
+
+    public void testFibonacci() {
+        assertEquals(1, App.fibonacci(1));
+        assertEquals(1, App.fibonacci(2));
+        assertEquals(55, App.fibonacci(10));
+        assertEquals(6765, App.fibonacci(20));
+        assertEquals(832040, App.fibonacci(30));
+        assertEquals(102334155, App.fibonacci(40));
+        assertEquals(2971215073L, App.fibonacci(47));
     }
 }
